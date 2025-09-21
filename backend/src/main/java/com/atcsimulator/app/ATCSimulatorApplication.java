8a2afe5b33b810784784ed7d4c7057cae569a834
Jackson Wozniak <jjw30@comcast.net>
2025-09-21 16:40:28 -0400

    initial commit of server

---
+++ backend/src/main/java/com/atcsimulator/app/ATCSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.atcsimulator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATCSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ATCSimulatorApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/atcsimulator/app/ATCSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atcsimulator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATCSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
